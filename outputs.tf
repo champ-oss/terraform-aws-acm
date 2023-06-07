5636d92f68eebee2d4372a168e6e4a1bc961ef20
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-06-07 18:34:17 -0500

    removing output as not needed

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,3 @@ output "arn" {
   description = "Certificate ARN"
   value       = aws_acm_certificate.this.arn
 }
-
-output "fqdn" {
-  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record#fqdn"
-  value       = aws_route53_record.this[0].fqdn
-}
