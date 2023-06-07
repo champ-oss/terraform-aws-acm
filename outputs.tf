bc950a2a4170fa99a7635a0ca0c104e3756105dd
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-06-07 16:04:54 -0500

    resolve lint

--- outputs.tf
+++ outputs.tf
@@ -6,5 +6,5 @@ output "arn" {
 
 output "fqdn" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record#fqdn"
-  value       = aws_route53_record.this.fqdn
+  value       = aws_route53_record.this[0].fqdn
 }
