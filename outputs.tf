abf1cda2946a550efdabe5f7ac838ef788152193
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-06-14 10:26:21 -0500

    adding arn minus sleep for testing

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,10 @@
 output "arn" {
+  depends_on  = [time_sleep.this]
+  description = "Certificate ARN"
+  value       = aws_acm_certificate.this.arn
+}
+
+output "arn_minus_sleep" {
   description = "Certificate ARN"
   value       = aws_acm_certificate.this.arn
 }
