451b7bca83aa62bf6d70868c73af6f316d278240
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-06-14 16:05:04 -0500

    removing fqdn

--- outputs.tf
+++ outputs.tf
@@ -8,8 +8,3 @@ output "arn_minus_sleep" {
   description = "Certificate ARN"
   value       = aws_acm_certificate.this.arn
 }
-
-output "fqdn" {
-  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record#fqdn"
-  value       = aws_route53_record.this[0].fqdn
-}
