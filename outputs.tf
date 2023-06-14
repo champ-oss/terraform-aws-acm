a9cde836d6980af80d61d2be04ba2a2e057c049e
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-06-14 15:57:16 -0500

    adding fqdn output var

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,8 @@ output "arn_minus_sleep" {
   description = "Certificate ARN"
   value       = aws_acm_certificate.this.arn
 }
+
+output "fqdn" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record#fqdn"
+  value       = aws_route53_record.this.fqdn
+}
