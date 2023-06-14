abda64a6c693bfb48a4b34bfa5c19ba15b64313d
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-06-14 15:51:31 -0500

    updating output vars

--- outputs.tf
+++ outputs.tf
@@ -4,11 +4,6 @@ output "arn" {
   value       = aws_acm_certificate.this.arn
 }
 
-output "fqdn" {
-  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record#fqdn"
-  value       = aws_route53_record.this.fqdn
-}
-
 output "arn_minus_sleep" {
   description = "Certificate ARN"
   value       = aws_acm_certificate.this.arn
