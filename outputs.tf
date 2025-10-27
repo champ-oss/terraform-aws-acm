31cb06fe408995ab0dd06ab0d7e7286e1b09a907
Phillip Langley <philliplangley@champmbplangley.lan>
2025-10-27 15:17:41 -0500

    output domain name

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "arn" {
   description = "Certificate ARN"
   value       = var.enable_validation ? try(aws_acm_certificate_validation.this[0].certificate_arn, "") : try(aws_acm_certificate.this[0].arn, "")
 }
+
+output "domain_name" {
+  description = "The domain name for which the certificate was issued"
+  value       = var.enabled ? aws_acm_certificate.this[0].domain_name : ""
+}
