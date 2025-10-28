17c69feda1a8691723db131ab2ed5c6a9e3185c1
Phillip Langley <philliplangley@champmbplangley.lan>
2025-10-28 09:40:36 -0500

    acm test with domain name

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "arn" {
-  description = "Certificate ARN"
+  description = "Certificate arn"
   value       = var.enable_validation ? try(aws_acm_certificate_validation.this[0].certificate_arn, "") : try(aws_acm_certificate.this[0].arn, "")
 }
 
